591cc49a9c33c80a99793b9547362fb562be65e3
Diego Vallejo <inge.diego94@gmail.com>
2025-11-13 21:53:21 -0500

    chore & docs(property,propertyDto, Services & controllers)

--- inmobiliariaApp.Domain/Entities/Property.cs
+++ inmobiliariaApp.Domain/Entities/Property.cs
@@ -3,12 +3,12 @@ using inmobiliariaApp.Domain.Entities;
 public class Property
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public decimal Price { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
+    public double Price { get; set; } 
+    public bool Available { get; set; }
+    public string Location { get; set; } = string.Empty;
+    public string? UrlClaudinary { get; set; } = string.Empty;
 
-   
-    public int? UsersId { get; set; }
-
-    public User? Users { get; set; }
 }
